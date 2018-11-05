822255abf8cacbc8e5b12b1eef6ea234db6bc182
MIRACLESHEEP <42984792+MIRACLESHEEP@users.noreply.github.com>
2018-11-05 15:02:34 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -7,7 +7,7 @@ function draw() {
 	background(100);
 	fill(66, 134, 244);
 	ellipse(210,300,50,50);
-getImage("creatures/Hopper-Happy");
+getImage( src = https://rmgamematerial.files.wordpress.com/2011/04/grassland01a_soruve.png?w=630);
 }
 /*
 http://i835.photobucket.com/albums/zz277/scanime/Modern%20Battlebacks/japan_street01.png
