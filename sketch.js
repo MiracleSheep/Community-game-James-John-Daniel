e80d6e239ff5233a98bba9b625e5470c1d181e96
MIRACLESHEEP <42984792+MIRACLESHEEP@users.noreply.github.com>
2018-11-08 13:42:19 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -6,63 +6,6 @@ function setup() {
 
 	img = loadImage("Community-game-James-John-Daniel/pictures/Startscreen.jpeg"); 
 }
-
-function draw() {  
-
-}
-var currentScene;
-
-var drawScene1 =function(){
-    currentScene = 1;
-
- background(100);
-	fill(66, 134, 244);
-	ellipse(210,300,50,50);
-};
-
-var drawScene5 =function(){
-    currentScene = 5;
-
-};
-
-var drawScene2 = function(){
-    currentScene = 2;
-background(1,1,1);
+function draw (){
 image(img, 100, 100);
-};
-
-
-var drawScene3 = function(){
-    currentScene = 3;
-
-
-};
-
-var drawScene4 = function(){
-    currentScene = 4;
-
-
-};
-
-drawScene1();
-
-
-mouseClicked=function(){
-    if(currentScene === 1){
-      drawScene2();  
-    }
-    else if(currentScene === 5){
-     drawScene1();  
-    }
-     else if(currentScene === 2){
-     drawScene3();  
-    }
-     else if(currentScene === 3){
-     drawScene4 ();
-     
-     }
-     else if(currentScene === 4){
-     drawScene5 ();
-     
-     }
-};
+}
