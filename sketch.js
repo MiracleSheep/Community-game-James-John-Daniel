f1192c7a52e412258db0407b3c34affe04201b10
MIRACLESHEEP <42984792+MIRACLESHEEP@users.noreply.github.com>
2018-11-12 13:22:45 -0500

    Update sketch.js
    trying to add startscreen image

--- sketch.js
+++ sketch.js
@@ -1,32 +1,29 @@
+
+
+var img;
+
 var currentScene = 1;
 function setup() {
   createCanvas(400, 400);
+  	img = loadImage("Community-game-James-John-Daniel/pictures/Startscreen.jpeg"); 
 }
 function draw(){
-//var currentScene;
 
 var drawScene1 =function(){
     currentScene = 1;
     background(200, 175, 175);
-   
-
-
-
+   image(img, 100, 100);
 
 };
-
 
 var drawScene2 = function(){
     currentScene = 2;
 background(199, 40, 199);
-
 };
-
 
 var drawScene3 = function(){
     currentScene = 3;
 background(0, 51, 255);
-
 
 };
 
@@ -37,9 +34,8 @@ background(255, 255, 255);
 var drawScene5 =function(){
     currentScene = 5;
     background(150, 150, 175);
-
-
 };
+  
 if(currentScene ===1){
 drawScene1();
 }
