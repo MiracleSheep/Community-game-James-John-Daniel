6c20f50380592b63510cd8becc258df75963a680
MIRACLESHEEP <42984792+MIRACLESHEEP@users.noreply.github.com>
2018-11-15 15:24:25 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,4 +1,45 @@
 
+	//this is the button function for the options
+	var optionButton = function(config) {
+    this.x = config.x || 0;
+    this.y = config.y || 0;
+    this.width = config.width || 84;
+    this.height = config.height || 50;
+    this.label = config.label || "Click";
+    this.color = config.color || color(207, 85, 85);
+    this.onClick = config.onClick || function() {};
+};
+
+
+optionButton.prototype.draw = function() {
+    if (this.isMouseInside() && mouseIsPressed) {
+        fill(255, 255, 255);
+    }
+    else {
+       fill(this.color); 
+    }
+    rectMode(CENTER);
+    rect(this.x, this.y, this.width, this.height, 5);
+    fill(0, 0, 0);
+    textSize(18);
+    textAlign(CENTER, CENTER);
+    text(this.label, this.x, this.y);
+};
+
+
+optionButton.prototype.isMouseInside = function() {
+    return mouseX > this.x-this.width/2 &&
+           mouseX < (this.x + this.width/2) &&
+           mouseY > this.y - this.height/2 &&
+           mouseY < (this.y + this.height/2);
+};
+
+
+optionButton.prototype.handleMouseClick = function() {
+    if (this.isMouseInside()) {
+		}
+}
+	    
 var img;
 var cnv;
 
@@ -41,46 +82,18 @@ background(0, 0, 0);
 	fill(250,0,0)
 	text("Game Options",125,50);
 	
-	//this is the button function for the options
-	var optionButton = function(config) {
-    this.x = config.x || 0;
-    this.y = config.y || 0;
-    this.width = config.width || 84;
-    this.height = config.height || 50;
-    this.label = config.label || "Click";
-    this.color = config.color || color(207, 85, 85);
-    this.onClick = config.onClick || function() {};
-};
-
-
-optionButton.prototype.draw = function() {
-    if (this.isMouseInside() && mouseIsPressed) {
-        fill(255, 255, 255);
-    }
-    else {
-       fill(this.color); 
-    }
-    rectMode(CENTER);
-    rect(this.x, this.y, this.width, this.height, 5);
-    fill(0, 0, 0);
-    textSize(18);
-    textAlign(CENTER, CENTER);
-    text(this.label, this.x, this.y);
-};
-
-
-optionButton.prototype.isMouseInside = function() {
-    return mouseX > this.x-this.width/2 &&
-           mouseX < (this.x + this.width/2) &&
-           mouseY > this.y - this.height/2 &&
-           mouseY < (this.y + this.height/2);
-};
-
-
-optionButton.prototype.handleMouseClick = function() {
-    if (this.isMouseInside()) {
-
-};
+var btn1 = new Button({
+/*
+add properties and draw buttons
+	x: ,
+    y: ,
+    width: ,
+    height: ,
+    color: color(),
+    label: 
+*/
+});
+		};
 	
 var drawScene3 = function(){
     currentScene = 3;
@@ -119,7 +132,4 @@ drawScene1();
      drawScene5 ();
      }
      
-};
-
-
-
+}
